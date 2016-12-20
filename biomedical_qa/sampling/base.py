9775a1be0c7873951a5028b9ef5cf14bddb4eb42
Georg Wiese <georgwiese@gmail.com>
2016-12-20 17:20:21 +0100

    Implement BaseSampler

---
+++ biomedical_qa/sampling/base.py
@@ -0,0 +1,61 @@
+import random
+import abc
+
+from nltk.tokenize import RegexpTokenizer
+
+
+class BaseSampler:
+
+    def __init__(self, batch_size, vocab, instances_per_epoch=None, shuffle=True):
+        self.__batch_size = batch_size
+        self.unk_id = vocab["<UNK>"]
+        self.start_id = vocab["<S>"]
+        self.end_id = vocab["</S>"]
+        self.vocab = vocab
+        self._instances_per_epoch = instances_per_epoch
+        self.num_batches = 0
+        self.epoch = 0
+        self._rng = random.Random(28739)
+        self.tokenizer = RegexpTokenizer(r'\w+|[^\w\s]')
+        self._qas, self.char_offsets = self.build_questions()
+
+        if shuffle:
+            self._rng.shuffle(self._qas)
+        if instances_per_epoch is not None:
+            self._qas = self._qas[:instances_per_epoch]
+        self._idx = 0
+
+
+    @abc.abstractmethod
+    def build_questions(self):
+        """Should return an Array of Questions and a char_offsets map."""
+        pass
+
+
+    def get_ids_and_offsets(self, s):
+        idxs = []
+        offsets = []
+        offset = 0
+        for t in self.tokenizer.tokenize(s):
+            offset = s.index(t, offset)
+            offsets.append(offset)
+            i = self.vocab.get(t, self.unk_id)
+            idxs.append(i)
+            offset += len(t)
+        return idxs, offsets
+
+
+    def get_batch(self):
+        qa_settings = [self._qas[i+self._idx] for i in range(min(self.__batch_size, len(self._qas) - self._idx))]
+        self._idx += len(qa_settings)
+
+        if self._idx == len(self._qas):
+            self.epoch += 1
+            self._rng.shuffle(self._qas)
+            self._idx = 0
+
+        return qa_settings
+
+
+    def reset(self):
+        self._idx = 0
--- biomedical_qa/sampling/squad.py
+++ biomedical_qa/sampling/squad.py
@@ -5,53 +5,39 @@ import random
 from nltk.tokenize import RegexpTokenizer
 
 from biomedical_qa.models import QASetting
+from biomedical_qa.sampling.base import BaseSampler
 
 
-class SQuADSampler:
+class SQuADSampler(BaseSampler):
+
     def __init__(self, dir, filenames, batch_size, vocab,
                  instances_per_epoch=None, shuffle=True, dataset_json=None,
-                 types=["factoid", "list"]):
-        self.__batch_size = batch_size
-        self.unk_id = vocab["<UNK>"]
-        self.start_id = vocab["<S>"]
-        self.end_id = vocab["</S>"]
-        self.vocab = vocab
-        self._instances_per_epoch = instances_per_epoch
-        self.num_batches = 0
-        self.epoch = 0
-        self._rng = random.Random(28739)
+                 types=None):
+
         if dataset_json is None:
             # load json
             with open(os.path.join(dir, filenames[0])) as dataset_file:
                 dataset_json = json.load(dataset_file)
-        dataset = dataset_json['data']
-        self._qas = []
+        self.dataset = dataset_json['data']
+        self.types = types if types is not None else ["factoid", "list"]
 
-        tokenizer = RegexpTokenizer(r'\w+|[^\w\s]')
+        BaseSampler.__init__(self, batch_size, vocab, instances_per_epoch, shuffle)
 
-        def trfm(s):
-            idxs = []
-            offsets = []
-            offset = 0
-            for t in tokenizer.tokenize(s):
-                offset = s.index(t, offset)
-                offsets.append(offset)
-                i = vocab.get(t, self.unk_id)
-                idxs.append(i)
-                offset += len(t)
-            return idxs, offsets
 
-        self.char_offsets = dict()
+    def build_questions(self):
 
-        for article in dataset:
+        char_offsets = dict()
+        qas = []
+
+        for article in self.dataset:
             for paragraph in article["paragraphs"]:
-                context, offsets = trfm(paragraph["context"])
+                context, offsets = self.get_ids_and_offsets(paragraph["context"])
                 for qa in paragraph["qas"]:
                     answers = []
                     answer_spans = []
                     answers_json = qa["answers"] if "answers" in qa else []
                     for a in answers_json:
-                        answer = trfm(a["text"])[0]
+                        answer = self.get_ids_and_offsets(a["text"])[0]
                         if answer and a["answer_start"] in offsets:
                             start = offsets.index(a["answer_start"])
                             if (start, start + len(answer)) in answer_spans:
@@ -61,33 +47,16 @@ class SQuADSampler:
 
                     q_type = qa["question_type"] if "question_type" in qa else None
                     is_yes = qa["answer_is_yes"] if "answer_is_yes" in qa else None
-                    if q_type is None or q_type in types:
-                        self._qas.append(QASetting(trfm(qa["question"])[0], answers,
-                                                   context, answer_spans,
-                                                   id=qa["id"],
-                                                   q_type=q_type,
-                                                   is_yes=is_yes,
-                                                   paragraph_json=paragraph,
-                                                   question_json=qa))
+                    if q_type is None or q_type in self.types:
+                        question_tokens = self.get_ids_and_offsets(qa["question"])[0]
+                        qas.append(QASetting(question_tokens, answers,
+                                             context, answer_spans,
+                                             id=qa["id"],
+                                             q_type=q_type,
+                                             is_yes=is_yes,
+                                             paragraph_json=paragraph,
+                                             question_json=qa))
 
-                    self.char_offsets[qa["id"]] = offsets
+                    char_offsets[qa["id"]] = offsets
 
-        if shuffle:
-            self._rng.shuffle(self._qas)
-        if instances_per_epoch is not None:
-            self._qas = self._qas[:instances_per_epoch]
-        self._idx = 0
-
-    def get_batch(self):
-        qa_settings = [self._qas[i+self._idx] for i in range(min(self.__batch_size, len(self._qas) - self._idx))]
-        self._idx += len(qa_settings)
-
-        if self._idx == len(self._qas):
-            self.epoch += 1
-            self._rng.shuffle(self._qas)
-            self._idx = 0
-
-        return qa_settings
-
-    def reset(self):
-        self._idx = 0
+        return qas, char_offsets
