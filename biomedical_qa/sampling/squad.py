884aa0a9b1f0c97cb79e813d8f2cf29d849f33f1
Georg Wiese <georgwiese@gmail.com>
2016-12-30 13:13:53 +0100

    SquadSampler: Implement list of list of answers

--- biomedical_qa/sampling/squad.py
+++ biomedical_qa/sampling/squad.py
@@ -57,22 +57,31 @@ class SQuADSampler(BaseSampler):
                     answers = []
                     answer_spans = []
                     answers_json = qa["answers"] if "answers" in qa else []
-                    for a in answers_json:
-                        answer, _ = self.get_ids_and_offsets(a["text"])
-                        if answer and a["answer_start"] in char_offset_to_token_index:
-                            context_index, start = char_offset_to_token_index[a["answer_start"]]
-                            end = start + len(answer)
-                            if (context_index, context_index, end) in answer_spans:
-                                continue
-                            answer_spans.append((context_index, start, end))
-                            answers.append(answer)
+                    answers_json_list = answers_json if isinstance(answers_json[0], list) \
+                                                     else [answers_json]
+                    for answer_list in answers_json_list:
+                        current_answer_spans = []
+                        current_answers = []
+
+                        for a in answer_list:
+                            answer, _ = self.get_ids_and_offsets(a["text"])
+                            if answer and a["answer_start"] in char_offset_to_token_index:
+                                context_index, start = char_offset_to_token_index[a["answer_start"]]
+                                end = start + len(answer)
+                                if (context_index, context_index, end) in answer_spans:
+                                    continue
+                                current_answer_spans.append((context_index, start, end))
+                                current_answers.append(answer)
+
+                        answer_spans.append(current_answer_spans)
+                        answers.append(current_answers)
 
                     q_type = qa["question_type"] if "question_type" in qa else None
                     is_yes = qa["answer_is_yes"] if "answer_is_yes" in qa else None
                     if q_type is None or q_type in self.types:
                         question_tokens = self.get_ids_and_offsets(qa["question"])[0]
-                        qas.append(QASetting(question_tokens, [answers],
-                                             contexts, [answer_spans],
+                        qas.append(QASetting(question_tokens, answers,
+                                             contexts, answer_spans,
                                              id=qa["id"],
                                              q_type=q_type,
                                              is_yes=is_yes,
