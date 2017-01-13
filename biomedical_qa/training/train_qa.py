55f84ccfd443bfe14639004af85449bf09d73045
Georg Wiese <georgwiese@gmail.com>
2017-01-13 11:50:31 +0100

    Bug fix

--- biomedical_qa/training/train_qa.py
+++ biomedical_qa/training/train_qa.py
@@ -88,9 +88,9 @@ config = tf.ConfigProto(allow_soft_placement=True)
 config.gpu_options.allow_growth = True
 
 
-def make_sampler(filenames, vocab, types):
+def make_sampler(dir, filenames, vocab, types):
     args = {
-        "dir": FLAGS.data,
+        "dir": dir,
         "filenames": filenames,
         "batch_size": FLAGS.batch_size,
         "vocab": vocab,
@@ -157,19 +157,19 @@ with tf.Session(config=config) as sess:
 
     print("Preparing Samplers ...")
     train_fns = [fn for fn in os.listdir(FLAGS.data) if fn.startswith(FLAGS.trainset_prefix)]
-    sampler = make_sampler(train_fns, model.transfer_model.vocab, ["factoid", "list"])
+    sampler = make_sampler(FLAGS.data, train_fns, model.transfer_model.vocab, ["factoid", "list"])
 
     valid_fns = [fn for fn in os.listdir(FLAGS.data) if fn.startswith(FLAGS.validset_prefix)]
-    valid_sampler = make_sampler(valid_fns, model.transfer_model.vocab, ["factoid", "list"])
+    valid_sampler = make_sampler(FLAGS.data, valid_fns, model.transfer_model.vocab, ["factoid", "list"])
 
     # Optionally load yes/no questions
     yesno_train_sampler = None
     yesno_valid_sampler = None
     if FLAGS.yesno_data is not None:
         train_fns = [fn for fn in os.listdir(FLAGS.yesno_data) if fn.startswith(FLAGS.trainset_prefix)]
-        yesno_train_sampler = make_sampler(train_fns, model.transfer_model.vocab, ["yesno"])
+        yesno_train_sampler = make_sampler(FLAGS.yesno_data, train_fns, model.transfer_model.vocab, ["yesno"])
         valid_fns = [fn for fn in os.listdir(FLAGS.yesno_data) if fn.startswith(FLAGS.validset_prefix)]
-        yesno_valid_sampler = make_sampler(valid_fns, model.transfer_model.vocab, ["yesno"])
+        yesno_valid_sampler = make_sampler(FLAGS.yesno_data, valid_fns, model.transfer_model.vocab, ["yesno"])
 
     if FLAGS.is_bioasq:
         main_trainer = BioAsqQATrainer(FLAGS.learning_rate, model, devices[0],
