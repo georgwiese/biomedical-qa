848a36cb15f5b99d112d74afebc13c7bdca3e0ae
Georg Wiese <georgwiese@gmail.com>
2017-01-31 11:42:44 +0100

    Implement extract all questions

--- biomedical_qa/tools/extract_pubmed_abstracts.py
+++ biomedical_qa/tools/extract_pubmed_abstracts.py
@@ -2,18 +2,19 @@ import tensorflow as tf
 import os
 import tarfile
 import json
+import nltk.data
 import xml.etree.ElementTree as ET
 import logging
+from html2text import html2text
 
 tf.app.flags.DEFINE_string('data_dir', None, 'Path to directory containing all tar.gz files.')
 tf.app.flags.DEFINE_string('out_json', None, 'Path to the output JSON file.')
+tf.app.flags.DEFINE_string('extract', "question_titles", '(question_titles|all_questions).')
 
 FLAGS = tf.app.flags.FLAGS
 
 
-def process_tarfile(tar):
-
-    data = []
+def iter_xmls(tar):
 
     members = tar.getmembers()
     for i, member in enumerate(members):
@@ -26,27 +27,58 @@ def process_tarfile(tar):
 
         with tar.extractfile(member) as f:
             root = ET.parse(f).getroot()
-            title_node = root.find("front/article-meta/title-group/article-title")
+            f.seek(0)
+            xml_text = str(f.read())
+            yield root, xml_text, member.name
 
-            if title_node is None or title_node.text is None:
-                logging.warning("No title: %s" % member.name)
-                continue
 
-            title = title_node.text
+def process_tarfile_question_titles(tar):
 
-            if title is None or title[-1] != "?":
-                continue
+    data = []
 
-            abstract_node = root.find("**/abstract")
-            if abstract_node is None:
-                logging.warning("No abstract: %s" % member.name)
-                continue
-            abstract_xml = ET.tostring(abstract_node).decode("utf-8")
+    for root, _, filename in iter_xmls(tar):
 
+        title_node = root.find("front/article-meta/title-group/article-title")
+
+        if title_node is None or title_node.text is None:
+            logging.warning("No title: %s" % filename)
+            continue
+
+        title = title_node.text
+
+        if title is None or title[-1] != "?":
+            continue
+
+        abstract_node = root.find("**/abstract")
+        if abstract_node is None:
+            logging.warning("No abstract: %s" % filename)
+            continue
+        abstract_xml = ET.tostring(abstract_node).decode("utf-8")
+
+        data.append({
+            "id": filename,
+            "title": title,
+            "abstract_xml": abstract_xml,
+        })
+
+    return data
+
+
+def process_tarfile_all_questions(tar):
+
+    data = []
+    tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
+
+    for _, xml_text, filename in iter_xmls(tar):
+
+        text = html2text(xml_text)
+        sentences = tokenizer.tokenize(text)
+        questions = [s for s in sentences if s[-1] == "?"]
+
+        for question in questions:
             data.append({
-                "id": member.name,
-                "title": title,
-                "abstract_xml": abstract_xml,
+                "filename": filename,
+                "question": question,
             })
 
     return data
@@ -55,6 +87,12 @@ def process_tarfile(tar):
 def main():
 
     data = []
+
+    process_tarfile = None
+    if FLAGS.extract == "question_titles":
+        process_tarfile = process_tarfile_question_titles
+    elif FLAGS.extract == "all_questions":
+        process_tarfile = process_tarfile_all_questions
 
     files = [f for f in os.listdir(FLAGS.data_dir) if f.endswith(".tar.gz")]
     for file in files:
