f168a0b8b4f86da65e9debded10d6abecda8ab1c
Georg Wiese <georgwiese@gmail.com>
2017-03-16 12:10:13 +0100

    BioASQ inference: make PreferredTermPreprocessor optional

--- biomedical_qa/tools/bioasq_inference.py
+++ biomedical_qa/tools/bioasq_inference.py
@@ -6,7 +6,7 @@ import tensorflow as tf
 from biomedical_qa.data.bioasq_squad_builder import BioAsqSquadBuilder
 from biomedical_qa.data.entity_tagger import get_entity_tagger
 from biomedical_qa.inference.inference import Inferrer, get_session, get_model
-from biomedical_qa.inference.postprocessing import DeduplicatePostprocessor, ProbabilityThresholdPostprocessor, TopKPostprocessor, PreferredTermPreprocessor
+from biomedical_qa.inference.postprocessing import NullPostprocessor, DeduplicatePostprocessor, ProbabilityThresholdPostprocessor, TopKPostprocessor, PreferredTermPreprocessor
 from biomedical_qa.sampling.squad import SQuADSampler
 
 tf.app.flags.DEFINE_string('bioasq_file', None, 'Path to the BioASQ JSON file.')
@@ -41,8 +41,13 @@ def insert_answers(bioasq_json, answers):
 
     questions = []
 
-    factoid_postprocessor = TopKPostprocessor(5)
-    list_postprocessor = ProbabilityThresholdPostprocessor(FLAGS.list_answer_prob_threshold, 1)
+    base_postprocessor = NullPostprocessor()
+    if FLAGS.terms_file is not None:
+        base_postprocessor = base_postprocessor.chain(PreferredTermPreprocessor(FLAGS.terms_file))
+    base_postprocessor = base_postprocessor.chain(DeduplicatePostprocessor())
+
+    factoid_postprocessor = base_postprocessor.chain(TopKPostprocessor(5))
+    list_postprocessor = base_postprocessor.chain(ProbabilityThresholdPostprocessor(FLAGS.list_answer_prob_threshold, 1))
 
     for question in bioasq_json["questions"]:
         q_id = question["id"]
@@ -86,9 +91,7 @@ if __name__ == "__main__":
 
     contexts = {p["qas"][0]["id"] : p["context_original_capitalization"]
                 for p in squad_json["data"][0]["paragraphs"]}
-    post_processor = PreferredTermPreprocessor(FLAGS.terms_file).chain(DeduplicatePostprocessor())
-    answers = {q_id: post_processor.process(inference_result)
-               for q_id, inference_result in inferrer.get_predictions(sampler).items()}
+    answers = inferrer.get_predictions(sampler).items()
     bioasq_json = insert_answers(bioasq_json, answers)
 
     os.makedirs(os.path.dirname(FLAGS.out_file), exist_ok=True)
